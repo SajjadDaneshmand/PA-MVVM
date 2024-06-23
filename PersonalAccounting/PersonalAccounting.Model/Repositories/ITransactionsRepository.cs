cc6b0c15598231f37463a33b42cfd6fbfe64b9e6
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-23 16:23:29 +0330

    create interface ITransactionsRepository: 1) add methods: GetAllTransactions, GetTransactionsByType, InsertTransaction, UpdateTransaction, DeleteTransaction, GetLargestTransaction, Save

---
+++ PersonalAccounting/PersonalAccounting.Model/Repositories/ITransactionsRepository.cs
@@ -0,0 +1,21 @@
+﻿using PersonalAccounting.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.Model.Repositories
+{
+    public interface ITransactionsRepository
+    {
+        List<Transactions> GetAllTransactions();
+        List<Transactions> GetTransactionsByType(int type);
+        bool InsertTransaction(Transactions transaction);
+        bool UpdateTransaction(Transactions transaction);
+        bool DeleteTransaction(Transactions transaction);
+        bool DeleteTransaction(int id);
+        Transactions GetLargestTransaction();
+        void Save();
+    }
+}
