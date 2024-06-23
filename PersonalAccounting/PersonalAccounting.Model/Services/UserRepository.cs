ec5f11660584c8f67fbfb52c0e7de839a3dfbedb
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-23 17:38:23 +0330

    create UserRepository. this file wrap User table

---
+++ PersonalAccounting/PersonalAccounting.Model/Services/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using PersonalAccounting.Model.Model;
+using PersonalAccounting.Model.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.Model.Services
+{
+    public class UserRepository : IUserRepository
+    {
+        private Accounting_DBEntities _db;
+
+        public UserRepository(Accounting_DBEntities db)
+        {
+            _db = db;
+        }
+
+        public bool DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UserExist()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
