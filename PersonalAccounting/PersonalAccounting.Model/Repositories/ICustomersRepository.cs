d424e622466e4d4c1586fb349d1cedb41a942a1b
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-23 16:21:38 +0330

    create interface ICustomerRepository: 1) add methods: GetAllCustomers, GetCustomerById, InsertCustomer, UpdateCustomer, DeleteCustomer, Save

---
+++ PersonalAccounting/PersonalAccounting.Model/Repositories/ICustomersRepository.cs
@@ -0,0 +1,20 @@
+﻿using PersonalAccounting.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.Model.Repositories
+{
+    public interface ICustomersRepository
+    {
+        List<Customers> GetAllCustomers();
+        Customers GetCustomerById(int customerId);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+        bool DeleteCustomer(Customers customer);
+        bool DeleteCustomer(int customerId);
+        void Save();
+    }
+}
