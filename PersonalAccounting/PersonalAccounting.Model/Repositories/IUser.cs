489d5f9150e7fa630313fd294cfbc13b2c272803
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-23 16:40:54 +0330

    create interface IUser: 1) add method: InsertUser, UpdateUser, DeleteUser, GetPassword, UserExist

---
+++ PersonalAccounting/PersonalAccounting.Model/Repositories/IUser.cs
@@ -0,0 +1,18 @@
+﻿using PersonalAccounting.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.Model.Repositories
+{
+    internal interface IUser
+    {
+        bool InsertUser(User user);
+        bool UpdateUser(User user);
+        bool DeleteUser(User user);
+        string GetPassword();
+        bool UserExist();
+    }
+}
