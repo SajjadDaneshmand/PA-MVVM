007ba48bfa36cf8e2b0e965352a201f3232d0086
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-24 11:52:15 +0330

    create folder Utilities that contain utils. and create file Constansts for my const variables

---
+++ PersonalAccounting/PersonalAccounting.Model/Utilities/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.Model.Utilities
+{
+    public static class Constants
+    {
+        public const int RECEIVE_TYPE = 1;
+        public const int PAYMENT_TYPE = 2;
+    }
+}
