24a3838dc948244de6aaf1976e530c4e42157ca1
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-24 11:48:40 +0330

    update interface IUserRepository => remove unused namespaces

--- PersonalAccounting/PersonalAccounting.Model/Repositories/IUserRepository.cs
+++ PersonalAccounting/PersonalAccounting.Model/Repositories/IUserRepository.cs
@@ -1,9 +1,5 @@
 ﻿using PersonalAccounting.Model.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PersonalAccounting.Model.Repositories
 {
