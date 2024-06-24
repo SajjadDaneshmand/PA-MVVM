6caf8fe58d98dd885d278d4541072b9b85a56873
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-24 11:47:32 +0330

    update ITransactionsRepository: 1) remove save method 2) add GetTransactionById method 3) divide GetLargetTransaction to two method: GetLargestPaymenTransactions and GetLargestReceiveTransactions and set their type to List<Transactions>

--- PersonalAccounting/PersonalAccounting.Model/Repositories/ITransactionsRepository.cs
+++ PersonalAccounting/PersonalAccounting.Model/Repositories/ITransactionsRepository.cs
@@ -1,21 +1,19 @@
 ﻿using PersonalAccounting.Model.Model;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PersonalAccounting.Model.Repositories
 {
     public interface ITransactionsRepository
     {
         List<Transactions> GetAllTransactions();
+        Transactions GetTransactionById(int id);
         List<Transactions> GetTransactionsByType(int type);
         bool InsertTransaction(Transactions transaction);
         bool UpdateTransaction(Transactions transaction);
         bool DeleteTransaction(Transactions transaction);
         bool DeleteTransaction(int id);
-        Transactions GetLargestTransaction();
-        void Save();
+        List<Transactions> GetLargestPaymentTransactions();
+        List<Transactions> GetLargestReceiveTransactions();
     }
 }
