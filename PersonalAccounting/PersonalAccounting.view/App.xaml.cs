a33d714a10568f505413a7c32a53471100f9fa5d
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-25 10:11:13 +0330

    configuration for use MaterialDesign

--- PersonalAccounting/PersonalAccounting.view/App.xaml.cs
+++ PersonalAccounting/PersonalAccounting.view/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PersonalAccounting.ViewModel.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace PersonalAccounting.view
     /// </summary>
     public partial class App : Application
     {
+        BaseViewModel x = new BaseViewModel();
     }
 }
