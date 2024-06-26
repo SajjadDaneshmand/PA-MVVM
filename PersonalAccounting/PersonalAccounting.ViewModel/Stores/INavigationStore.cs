feadc46279d38274edea0321df8457abf6db5145
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-26 17:52:07 +0330

    Create Store folder that have NavigationStore and its interface. NavigationStore is for hold current ViewModel and be available in all Projects from this solution

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Stores/INavigationStore.cs
@@ -0,0 +1,16 @@
+﻿using PersonalAccounting.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Stores
+{
+    public interface INavigationStore
+    {
+        IBaseViewModel CurrentViewModel { get; set;  }
+
+        event Action CurrentViewModelChanged;
+    }
+}
