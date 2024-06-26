b52df0da59b001c864141ec65b4fdf6c8ddce12e
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-26 15:46:13 +0330

    create ContainerConfig that create and hold instances using by Autofac

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/ContainerConfig.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Core;
+
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using PersonalAccounting.ViewModel.ViewModels.ViewModels;
+using PersonalAccounting.ViewModel;
+using PersonalAccounting.ViewModel.ViewModels;
+
+namespace PersonalAccounting.ViewModel
+{
+    public class ContainerConfig
+    {
+        public static Autofac.IContainer Configure(object parametrs)
+        {
+            var builder = new ContainerBuilder();
+
+            // Register ViewModels and their interfaces
+            builder.RegisterType<BaseViewModel>().As<IBaseViewModel>();
+            builder.RegisterType<MainWindowViewModel>().As<IMainWindowViewModel>().SingleInstance();
+            builder.RegisterType<HomeViewModel>().As<IHomeViewModel>().SingleInstance();
+
+            // Register Commands
+            
+            // Register Services
+
+            // Register Stores
+
+
+            return builder.Build();
+        }
+    }
+}
