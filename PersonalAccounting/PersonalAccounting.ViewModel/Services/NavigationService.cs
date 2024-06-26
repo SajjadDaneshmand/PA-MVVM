8888bb23a41c50b034124e5d6d0065c69ac3e8ca
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-26 17:53:59 +0330

    Create Services folder that have NavigationService and its interface. NavigationService is service for NavigationStore for manage hold currentViewModel in that

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationService.cs
@@ -0,0 +1,28 @@
+﻿using PersonalAccounting.ViewModel.ViewModels;
+using PersonalAccounting.ViewModel.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services
+{
+    public class NavigationService : INavigationService
+    {
+        private INavigationStore _navigationStore;
+        private IBaseViewModel _createViewModel;
+
+        public NavigationService(INavigationStore navigationStore, IBaseViewModel createViewModel)
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _createViewModel;
+        }
+    }
+}
