8f18549aa987c7d5616a8d4841f95c497dd09b45
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-27 12:09:10 +0330

    create HomeNavigationService and its interface for navigate to home page

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/HomeNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class HomeNavigationService : IHomeNavigationService
+    {
+        IBaseViewModel _targetViewModel;
+        INavigationStore _navigationStore;
+
+        public HomeNavigationService(IBaseViewModel targetViewModel, INavigationStore navigationStore)
+        {
+            _targetViewModel = targetViewModel;
+            _navigationStore = navigationStore;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _targetViewModel;
+        }
+    }
+}
