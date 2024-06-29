80b4a6b6d4273357da0096ebf2149b707afbed2e
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:02:20 +0330

    Create ReportsVirwModel

--- PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/HomeViewModel.cs
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/HomeViewModel.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
 {
-    public class HomeViewModel
+    public class HomeViewModel : BaseViewModel, IHomeViewModel
     {
+        public HomeViewModel(ITestCommand testCommand)
+        {
+            TestCommand = testCommand;
+        }
+
+        public ITestCommand TestCommand { get; }
+
     }
 }
--- PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/MainWindowViewModel.cs
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,44 @@ using System.Threading.Tasks;
 
 namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
 {
-    internal class MainWindowViewModel
+    public class MainWindowViewModel : BaseViewModel, IMainWindowViewModel
     {
+        // Store for managing navigation
+        private readonly INavigationStore _navigationStore;
+
+
+        public IBaseViewModel CurrentViewModel => _navigationStore.CurrentViewModel;
+
+        public MainWindowViewModel(
+            INavigationStore navigationStore,
+            IHomeNavigationService homeNavigationService,
+            INavigateToHomeCommand navigateToHomeCommand,
+            INavigateToPersonsCommand navigateToPersonsCommand,
+            INavigateToTransactionsCommand navigateToTransactionsCommand,
+            INavigateToReportsCommnad navigateToReportsCommnad,
+            INavigateToSettingsCommand navigateToSettingsCommand)
+        {
+            _navigationStore = navigationStore;
+            NavigateToHomeCommand = navigateToHomeCommand;
+            NavigateToPersonsCommand = navigateToPersonsCommand;
+            NavigateToTransactionsCommand = navigateToTransactionsCommand;
+            NavigateToReportsCommand = navigateToReportsCommnad;
+            NavigateToSettingsCommand = navigateToSettingsCommand;
+
+            _navigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+
+        // Commands
+        public INavigateToHomeCommand NavigateToHomeCommand {  get; }
+        public INavigateToPersonsCommand NavigateToPersonsCommand { get; }
+        public INavigateToTransactionsCommand NavigateToTransactionsCommand { get; }
+        public INavigateToReportsCommnad NavigateToReportsCommand { get; }
+        public INavigateToSettingsCommand NavigateToSettingsCommand { get; }
+
     }
 }
