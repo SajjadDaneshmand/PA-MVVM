d2b5defc7c489b620ee1458b5dda6516ab756de3
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:04:57 +0330

    Create interface for PersonsNavigatiobService

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationInterfaces/IPersonsNavigationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationInterfaces
+{
+    public interface IPersonsNavigationService
+    {
+        void Navigate();
+    }
+}
