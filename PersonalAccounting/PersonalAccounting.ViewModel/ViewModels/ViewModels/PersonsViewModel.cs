90b23fc33243faff9a343ad5a237d254e1e2bcc1
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:01:59 +0330

    Create PersonsViewModel

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/PersonsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
+{
+    public class PersonsViewModel: BaseViewModel, IPersonsViewModel
+    {
+    }
+}
