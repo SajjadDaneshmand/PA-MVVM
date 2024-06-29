10e2bc288bab2b9f8695511e2f59ecd7713be8d1
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 14:44:29 +0330

    Create NewPersonViewModel for connect to NewPersonUserControl

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/IViewModels/INewPersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.ViewModels.IViewModels
+{
+    public interface INewPersonViewModel: IBaseViewModel
+    {
+    }
+}
---
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/NewPersonViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.ViewModels
+{
+    public class NewPersonViewModel: BaseViewModel, INewPersonViewModel
+    {
+    }
+}
