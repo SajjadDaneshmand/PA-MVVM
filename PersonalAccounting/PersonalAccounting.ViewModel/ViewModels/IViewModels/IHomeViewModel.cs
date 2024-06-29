0a95cee0b59c6907a79d42337fadee3d27c60191
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 09:48:39 +0330

    update IHomeViewModel: 1) add IBaseViewModel as parent of IHomeViewModel

--- PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/IViewModels/IHomeViewModel.cs
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/IViewModels/IHomeViewModel.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace PersonalAccounting.ViewModel.ViewModels.IViewModels
 {
-    public interface IHomeViewModel
+    public interface IHomeViewModel: IBaseViewModel
     {
+
     }
 }
