9e92f5468e190c36b4d8802d203c5a396da12be8
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:11:32 +0330

    create TransactionsNavigationService for navigate between userControls

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/TransactionsNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class TransactionsNavigationService: ITransactionsNavigationService
+    {
+        private readonly INavigationStore _navigationStore;
+        private readonly ITransactionsViewModel _transactionsViewModel;
+
+        public TransactionsNavigationService(INavigationStore navigationStore, ITransactionsViewModel transactionsViewModel)
+        {
+            _navigationStore = navigationStore;
+            _transactionsViewModel = transactionsViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _transactionsViewModel;
+        }
+    }
+}
