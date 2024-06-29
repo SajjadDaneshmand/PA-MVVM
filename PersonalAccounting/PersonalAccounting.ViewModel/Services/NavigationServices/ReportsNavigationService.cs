0870f181362dd5e9ddff1959240f3df584c74da0
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:10:01 +0330

    create ReportsNavigationService for navigate between userControls

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/ReportsNavigationService.cs
@@ -0,0 +1,29 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+
+    public class ReportsNavigationService : IReportsNavigationService
+    {
+        private readonly INavigationStore _navigationStore;
+        private readonly IReportsViewModel _reportsViewModel;
+
+        public ReportsNavigationService(INavigationStore navigationStore, IReportsViewModel reportsViewModel)
+        {
+            _navigationStore = navigationStore;
+            _reportsViewModel = reportsViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _reportsViewModel;
+        }
+    }
+}
