f94adb5f0e7bc6124aa014e2aa4a7965fbcf96c7
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:15:00 +0330

    create TestCommand for test buttons working fine

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Commands/Commands/TestCommand.cs
@@ -0,0 +1,18 @@
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalAccounting.ViewModel.Commands.Commands
+{
+    public class TestCommand : BaseCommand, ITestCommand
+    {
+        public override void Execute(object parameter)
+        {
+            MessageBox.Show("It Works!!");
+        }
+    }
+}
