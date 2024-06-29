7b30e7b85e032992a3aa1d9938c9df03818fc7cb
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:08:44 +0330

    Create PersonsNavigationService for navigate between userControls

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/PersonsNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class PersonsNavigationService : IPersonsNavigationService
+    {
+        private readonly INavigationStore _navigationStore;
+        private readonly IPersonsViewModel _personsViewModel;
+
+        public PersonsNavigationService(INavigationStore navigationStore, IPersonsViewModel personsViewModel)
+        {
+            _navigationStore = navigationStore;
+            _personsViewModel = personsViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _personsViewModel;
+        }
+    }
+}
