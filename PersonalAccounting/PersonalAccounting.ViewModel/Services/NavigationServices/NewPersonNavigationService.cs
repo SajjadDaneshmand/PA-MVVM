ef4af65ec02d44a6a95fb371828e361f93060573
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 14:45:31 +0330

    create navigation service for NewPersonUserControl

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/NewPersonNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class NewPersonNavigationService : INewPersonNavigationService
+    {
+        private readonly INavigationStore _navigationStore;
+        private readonly INewPersonViewModel _newPersonViewModel;
+
+        public NewPersonNavigationService(INavigationStore navigationStore, INewPersonViewModel newPersonViewModel)
+        {
+            _navigationStore = navigationStore;
+            _newPersonViewModel = newPersonViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _newPersonViewModel;
+        }
+    }
+}
