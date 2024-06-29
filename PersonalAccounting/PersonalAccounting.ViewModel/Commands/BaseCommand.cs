fae9ec01a7bcaa0340004a14d3622595c5f6a62b
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:15:58 +0330

    create BaseCommand class. it's base of my commands

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Commands/BaseCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PersonalAccounting.ViewModel.Commands
+{
+    public abstract class BaseCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object parameter);
+        public virtual async Task ExecuteAsync(object parameter)
+        {
+            await Task.Run(() => Execute(parameter));
+        }
+
+
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
