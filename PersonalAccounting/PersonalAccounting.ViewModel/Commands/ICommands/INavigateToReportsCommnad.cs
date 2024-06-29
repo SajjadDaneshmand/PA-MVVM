0fa67b32767d64046a68d2f7e90350fcdbc9a603
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:12:49 +0330

    create interface for navigation commands

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Commands/ICommands/INavigateToReportsCommnad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Commands.ICommands
+{
+    public interface INavigateToReportsCommnad
+    {
+        void Execute(object parameter);
+    }
+}
