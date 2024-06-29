065f081df36c386c09ba8284d5d227227ecfe8b2
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-29 10:10:43 +0330

    create SettingsNavigationService for navigate between userControls

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/SettingsNavigationService.cs
@@ -0,0 +1,29 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class SettingsNavigationService: ISettingsNavigationService
+    {
+        private readonly INavigationStore _navigationStore;
+        private readonly ISettingsViewModel _settingsViewModel;
+
+        public SettingsNavigationService(INavigationStore navigationStore, ISettingsViewModel settingsViewModel)
+        {
+            _navigationStore = navigationStore;
+            _settingsViewModel = settingsViewModel;
+        }
+
+        public void Navigate()
+        {
+            _navigationStore.CurrentViewModel = _settingsViewModel;
+        }
+    }
+
+}
