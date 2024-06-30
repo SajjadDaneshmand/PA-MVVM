594737f2f3c60e05ef7dc036160d99bcedb2aa17
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 19:49:10 +0330

    create view model for two new page i want to create
    - first is about  new transaction and second is about transactions list

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/NewTransactionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
+{
+    public class NewTransactionViewModel: BaseViewModel, INewTransactionViewModel
+    {
+    }
+}
