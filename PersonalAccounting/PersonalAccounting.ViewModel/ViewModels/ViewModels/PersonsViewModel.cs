62ae181e5407ef77dfcc9a8f679c3989b8a51c30
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 10:40:25 +0330

    Configure PersonsViewModel for tab movement

--- PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/PersonsViewModel.cs
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/PersonsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
 
 using System;
 using System.Collections.Generic;
@@ -10,5 +12,30 @@ namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
 {
     public class PersonsViewModel: BaseViewModel, IPersonsViewModel
     {
+        private readonly IPersonsTabNavigationStore _personsTabNavigationStore;
+        public IBaseViewModel CurrentViewModel => _personsTabNavigationStore.CurrentViewModel;
+        
+
+        public PersonsViewModel(
+            INavigateToNewPersonCommand navigateToNewPersonCommand,
+            INavigateToPersonsListCommand navigateToPersonsListCommand,
+            IPersonsTabNavigationStore personsTabNavigationStore
+            )
+        {
+            NavigateToNewPersonCommand = navigateToNewPersonCommand;
+            NavigateToPersonsListCommand = navigateToPersonsListCommand;
+            _personsTabNavigationStore = personsTabNavigationStore;
+
+            _personsTabNavigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+
+        public INavigateToNewPersonCommand NavigateToNewPersonCommand { get; }
+        public INavigateToPersonsListCommand NavigateToPersonsListCommand { get; }
     }
 }
