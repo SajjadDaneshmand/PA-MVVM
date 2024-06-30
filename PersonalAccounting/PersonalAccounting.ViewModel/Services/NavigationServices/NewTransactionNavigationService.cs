9d89f09298d56f7de898b0a7744fc0052c90b1f4
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 19:50:39 +0330

    create navigation service for them
    - create navigation service for my two view model
    - NewTransactionViewModel and TransactionsListViewModel

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/NewTransactionNavigationService.cs
@@ -0,0 +1,30 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class NewTransactionNavigationService : INewTransactionNavigationService
+    {
+        private readonly INewTransactionViewModel _newTransactionViewModel;
+        private readonly ITransactionsTabNavigationStore _transactionsTabNavigationStore;
+
+        public NewTransactionNavigationService(
+            INewTransactionViewModel newTransactionViewModel,
+            ITransactionsTabNavigationStore transactionsTabNavigationStore)
+        {
+            _newTransactionViewModel = newTransactionViewModel;
+            _transactionsTabNavigationStore = transactionsTabNavigationStore;
+        }
+
+        public void Navigate()
+        {
+            _transactionsTabNavigationStore.CurrentViewModel = _newTransactionViewModel;
+        }
+    }
+}
---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/TransactionsListNavigationService.cs
@@ -0,0 +1,30 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class TransactionsListNavigationService : ITransactionsListNavigationService
+    {
+        private readonly ITransactionsTabNavigationStore _transactionsTabNavigationStore;
+        private readonly ITransactionsListViewModel _transactionsListViewModel;
+
+        public TransactionsListNavigationService(
+            ITransactionsTabNavigationStore transactionsTabNavigationStore,
+            ITransactionsListViewModel transactionsListViewModel)
+        {
+            _transactionsTabNavigationStore = transactionsTabNavigationStore;
+            _transactionsListViewModel = transactionsListViewModel;
+        }
+
+        public void Navigate()
+        {
+            _transactionsTabNavigationStore.CurrentViewModel = _transactionsListViewModel;
+        }
+    }
+}
