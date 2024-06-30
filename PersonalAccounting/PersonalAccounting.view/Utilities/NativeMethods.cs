2f8d73145803ba6cf4a2eadfba0d9ccc0b1752cf
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 11:05:22 +0330

    create Folder Utilities got our utilities and also create NstiveMethods class inside this folder for contract with system api. this file comunicate with windows apis to set title bar to dark mode

---
+++ PersonalAccounting/PersonalAccounting.view/Utilities/NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.view.Utilities
+{
+    public class NativeMethods
+    {
+        public const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+        [DllImport("dwmapi.dll", SetLastError = true)]
+        public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+    }
+}
