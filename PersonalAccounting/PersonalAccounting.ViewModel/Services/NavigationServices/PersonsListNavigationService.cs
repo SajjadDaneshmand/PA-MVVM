6712c355d52a8cec5b2bfb2a5e7b809b21c94c51
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 10:19:04 +0330

    create service for navigate to PersonsList with name PersonsListNavigationService

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Services/NavigationServices/PersonsListNavigationService.cs
@@ -0,0 +1,28 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Services.NavigationServices
+{
+    public class PersonsListNavigationService : IPersonsListNavigationService
+    {
+        private readonly IPersonsTabNavigationStore _tabNavigationStore;
+        private readonly IPersonsListViewModel _personsListViewModel;
+
+        public PersonsListNavigationService(IPersonsTabNavigationStore tabNavigationStore, IPersonsListViewModel personsListViewModel)
+        {
+            _tabNavigationStore = tabNavigationStore;
+            _personsListViewModel = personsListViewModel;
+        }
+
+        public void Navigate()
+        {
+            _tabNavigationStore.CurrentViewModel = _personsListViewModel;
+        }
+    }
+}
