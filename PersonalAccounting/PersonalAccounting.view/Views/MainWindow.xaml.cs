e40823cd889d4f3e614558cd2efda5825b10de76
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 11:06:05 +0330

    use of NativeMethods in my MainWindow to set title bar to dark mode

--- PersonalAccounting/PersonalAccounting.view/Views/MainWindow.xaml.cs
+++ PersonalAccounting/PersonalAccounting.view/Views/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +9,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PersonalAccounting.view.Utilities;
 
 namespace PersonalAccounting.view.Views
 {
@@ -23,6 +26,16 @@ namespace PersonalAccounting.view.Views
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = (Window)sender;
+            var hwnd = new WindowInteropHelper(window).Handle;
+
+            int useImmersiveDarkMode = 1; // Enable dark mode
+            NativeMethods.DwmSetWindowAttribute(hwnd, NativeMethods.DWMWA_USE_IMMERSIVE_DARK_MODE, ref useImmersiveDarkMode, sizeof(int));
         }
     }
 }
