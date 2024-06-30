07cfbf39ba5aa1041239ef7d1f0afe45d8596b38
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-06-30 10:20:37 +0330

    create command for navigate to PersonsList with name NavigateToPersonsListCommand

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Commands/Commands/NavigateToPersonsListCommand.cs
@@ -0,0 +1,26 @@
+﻿using PersonalAccounting.ViewModel.Services.NavigationServices;
+using PersonalAccounting.ViewModel.Commands.ICommands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalAccounting.ViewModel.Services.NavigationInterfaces;
+
+namespace PersonalAccounting.ViewModel.Commands.Commands
+{
+    public class NavigateToPersonsListCommand : BaseCommand, INavigateToPersonsListCommand
+    {
+        private readonly IPersonsListNavigationService _personsListNavigationService;
+
+        public NavigateToPersonsListCommand(IPersonsListNavigationService personsListNavigationService)
+        {
+            _personsListNavigationService = personsListNavigationService;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _personsListNavigationService.Navigate();
+        }
+    }
+}
