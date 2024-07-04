0a40e529c45dc6a43eca7d2d06a9b81f4c6a9036
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-04 20:21:12 +0330

    create TransactionsListUserControl

---
+++ PersonalAccounting/PersonalAccounting.view/UserControls/TransactionsChild/TransactionsListUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PersonalAccounting.view.UserControls.TransactionsChild
+{
+    /// <summary>
+    /// Interaction logic for TransactionsListUserControl.xaml
+    /// </summary>
+    public partial class TransactionsListUserControl : UserControl
+    {
+        public TransactionsListUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
