1262be832d4bae715b76f501578533deb6a30d2c
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-04 18:44:46 +0330

    manage tabNavgation for transaction
    - get commands and store in a constructor

--- PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/TransactionsViewModel.cs
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/TransactionsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using PersonalAccounting.ViewModel.Stores;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,30 @@ namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
 {
     public class TransactionsViewModel: BaseViewModel, ITransactionsViewModel
     {
+        private readonly ITransactionsTabNavigationStore _transactionsTabNavigationStore;
+        public IBaseViewModel CurrentViewModel => _transactionsTabNavigationStore.CurrentViewModel;
+
+
+        public TransactionsViewModel(
+            INavigateToNewTransactionCommand navigateToNewTransactionCommand,
+            INavigateToTransactionsListCommand navigateToTransactionsListCommand,
+            ITransactionsTabNavigationStore transactionsTabNavigationStore
+            )
+        {
+            NavigateToNewTransactionCommand = navigateToNewTransactionCommand;
+            NavigateToTransactionsListCommand = navigateToTransactionsListCommand;
+            _transactionsTabNavigationStore = transactionsTabNavigationStore;
+
+            _transactionsTabNavigationStore.CurrentViewModelChanged += OnCurrentViewModelChanged;
+
+        }
+
+        private void OnCurrentViewModelChanged()
+        {
+            OnPropertyChanged(nameof(CurrentViewModel));
+        }
+
+        public INavigateToNewTransactionCommand NavigateToNewTransactionCommand { get; }
+        public INavigateToTransactionsListCommand NavigateToTransactionsListCommand { get; }
     }
 }
