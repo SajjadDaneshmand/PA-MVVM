c8f6686337b1297e68fd9027233cd2d97f9bf00b
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-08 13:33:16 +0330

    show persons datagrid and filter it

--- PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/PersonsListViewModel.cs
+++ PersonalAccounting/PersonalAccounting.ViewModel/ViewModels/ViewModels/PersonsListViewModel.cs
@@ -3,6 +3,7 @@ using PersonalAccounting.Model.Model;
 using PersonalAccounting.ViewModel.ViewModels.IViewModels;
 using PersonalAccounting.ViewModel.ViewModels.Models;
 using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -13,16 +14,38 @@ namespace PersonalAccounting.ViewModel.ViewModels.ViewModels
 {
     public class PersonsListViewModel: BaseViewModel, IPersonsListViewModel
     {
-        public List<Customers> PersonsCollection
+        private string _searchInput;
+        private ObservableCollection<Customers> _personCollection;
+
+        public PersonsListViewModel()
         {
-            get
+         
+        }
+        public string SearchInput {
+            get => _searchInput;
+            set
             {
-                using (var db = new UnitOfWork())
-                {
-                    return db.CustomersRepository.GetAllCustomers();
-                }
+                _searchInput = value;
+                OnPropertyChanged(nameof(SearchInput));
+                UpdatePersonList();
             }
         }
+        public ObservableCollection<Customers> PersonsCollection {
+            get => _personCollection;
+            set
+            {
+                SetField(ref _personCollection, value, nameof(PersonsCollection));
+            }
+        }
+
+        private void UpdatePersonList()
+        {
+            using (var db = new UnitOfWork())
+            {
+                PersonsCollection = db.CustomersRepository.FilterCustomersByNameMobile(SearchInput);
+            }
+        }
+
 
     }
 }
