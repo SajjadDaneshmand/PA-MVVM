8f62808cf603557cb7f5d86c70cda95f1135438a
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-11 14:42:22 +0330

    create Constants class
    - this class hold settings and things like thaat for PersonalAccounting.ViewModel

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel
+{
+    public static class Constants
+    {
+        public static string START_DATE_REGISTRY_KEY = "StartDate";
+    }
+}
