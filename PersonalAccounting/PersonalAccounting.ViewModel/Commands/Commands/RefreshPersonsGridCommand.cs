d0c3162840a2afef39a298fe5a6b28d3c93d6273
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-11 14:30:13 +0330

    create command RefreshPersonsGridCommand
    - this command will refresh datagrid in PersonsListUserControl

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Commands/Commands/RefreshPersonsGridCommand.cs
@@ -0,0 +1,20 @@
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Commands.Commands
+{
+    public class RefreshPersonsGridCommand : BaseCommand, IRefreshPersonsGridCommand
+    {
+        public IPersonsListViewModel PersonsListViewModel;
+
+        public override void Execute(object parameter)
+        {
+            PersonsListViewModel.RefreshGrid();
+        }
+    }
+}
