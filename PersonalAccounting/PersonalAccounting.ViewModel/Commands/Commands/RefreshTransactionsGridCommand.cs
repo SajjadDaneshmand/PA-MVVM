4ca83db283af049006a6a9afdb6eeccb43ba3d52
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-11 14:32:49 +0330

    create command RefreshTransactionsGridCommand
    - this class refresh dataGrid in TransactionsListUserControl

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Commands/Commands/RefreshTransactionsGridCommand.cs
@@ -0,0 +1,19 @@
+﻿using PersonalAccounting.ViewModel.Commands.ICommands;
+using PersonalAccounting.ViewModel.ViewModels.IViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Commands.Commands
+{
+    public class RefreshTransactionsGridCommand : BaseCommand, IRefreshTransactionsGridCommand
+    {
+        public ITransactionsListViewModel TransactionsListViewModel;
+        public override void Execute(object parameter)
+        {
+            TransactionsListViewModel.RefreshTrDataGrid();
+        }
+    }
+}
