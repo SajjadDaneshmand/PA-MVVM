280d6ff50768a784bd1695f7867c88850a3d17e8
SajjadDaneshmand <Mr.SajjadDaneshmand@gmail.com>
2024-07-11 14:41:10 +0330

    create AccountingRegistry utility
    - this class working with registry windows but register as PA app

---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Utilities/AccountingRegistry.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Utilities
+{
+    public class AccountingRegistry : IAccountingRegistry
+    {
+        private readonly string _registryName = "SOFTWARE\\PA";
+        private RegistryKey _registryKey;
+        private bool _isRegistryExist = true;
+
+        public bool IsRegistryExist
+        {
+            get => _isRegistryExist;
+            set
+            {
+                _isRegistryExist = value;
+            }
+        }
+        public string RegisteryName => _registryName;
+
+        public AccountingRegistry()
+        {
+
+            _registryKey = Registry.CurrentUser.OpenSubKey(_registryName);
+            if (_registryKey == null)
+            {
+                _registryKey = Registry.CurrentUser.CreateSubKey(_registryName);
+                IsRegistryExist = false;
+            }
+        }
+
+        public string Get(string key)
+        {
+            return _registryKey.GetValue(key, null).ToString();
+        }
+
+
+        public bool Write(string key, string value)
+        {
+            _registryKey.SetValue(key, value);
+            return true;
+        }
+    }
+}
---
+++ PersonalAccounting/PersonalAccounting.ViewModel/Utilities/IAccountingRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalAccounting.ViewModel.Utilities
+{
+    public interface IAccountingRegistry
+    {
+        string RegisteryName { get; }
+        bool IsRegistryExist { get; set; }
+        bool Write(string key, string value);
+        string Get(string key);
+    }
+}
